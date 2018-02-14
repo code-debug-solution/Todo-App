32318ba445b625c55a2a79e347c7edaa3363292f
rodcode47 <rodney.hammad@gmail.com>
2018-02-14 11:32:16 +0200

    implement login-auth commit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const environment = require('./src/environments/environment.prod');
+
 const app = express();
 
 app.use(express.static(__dirname + "/dist"));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
    firebaseConfig: {
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
    }
 };
